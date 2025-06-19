a553465fff4a997c52a1361e5f63d1e534264854
Maosheng Jiang <m.jiang-5@student.tudelft.nl>
2025-06-19 17:20:19 +0200

    w

---
+++ README.md
@@ -0,0 +1,4 @@
+# Notes
+
+https://matplotlib.org/stable/users/explain/figure/event_handling.html
+https://stackoverflow.com/questions/63897367/how-to-increase-slider-length-in-ipywidgets
--- main.py
+++ main.py
@@ -13,4 +13,26 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.set_title('click on points')
+
+    line, = ax.plot(np.random.rand(100), 'o', pickradius=5)  # 5 points tolerance
+
+
+    def onpick(event):
+        thisline = event.artist
+        xdata = thisline.get_xdata()
+        ydata = thisline.get_ydata()
+        ind = event.ind
+        points = tuple(zip(xdata[ind], ydata[ind]))
+        print('onpick points:', points)
+
+
+    fig.canvas.mpl_connect('pick_event', onpick)
+
+    plt.show()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
